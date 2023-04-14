add78e0897fca3bf169222510cb36261bf6289d1
mercurywang <mercwang@hotmail.com>
2023-04-14 21:09:01 +0900

    feat: add webpack

--- part7/ultimate-hooks/src/fetch.js
+++ part7/ultimate-hooks/src/fetch.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from 'axios';
 
 const getAll = async (baseUrl) => {
   const response = await axios.get(baseUrl);
---
+++ webpack/src/index.js
@@ -0,0 +1,7 @@
+import "core-js/stable/index.js";
+import "regenerator-runtime/runtime.js";
+import App from "./App";
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.createRoot(document.getElementById("root")).render(<App />);

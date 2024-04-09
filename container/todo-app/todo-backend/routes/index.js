712fb7d5ffa24ee988252336f44ab58b0d7284bf
mercurywang <mercwang@hotmail.com>
2024-04-09 17:16:32 +0900

    feat: exercise 12.7 mongo db coding

--- container/todo-app/todo-backend/routes/index.js
+++ container/todo-app/todo-backend/routes/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const configs = require('../util/config')
 
+const Todo = require("../mongo/models/Todo")
+
 let visits = 0
 
 /* GET index data. */
@@ -14,5 +16,21 @@ router.get('/', async (req, res) => {
     visits
   });
 });
+
+/* GET todo by id */
+router.get("/todos/:id", async(req, res)=>{
+  const todo = await Todo.findById(req.params.id)
+  if (todo) {
+    res.json(todo)
+  } else {
+    res.status(404).end()
+  }
+})
+
+/* PUT: update todo by id */
+router.put("/todos/:id", async(req, res)=>{
+  const updated = await Todo.findByIdAndUpdate(req.params.id, req.body, {new: true})
+  res.status(201).json(updated)
+})
 
 module.exports = router;
